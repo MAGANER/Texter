679daed3895172872997df05ed0daa53d26d5689
MAGANER <maganer.krik@mail.ru>
2019-06-01 10:12:32 +0300

    line that is printed vertically

---
+++ VerticalLine.cpp
@@ -0,0 +1,16 @@
+#include "VerticalLine.h"
+
+VerticalLine::VerticalLine(int length, string _char, Position position) :
+	BaseLine(length,_char,position)
+{
+}
+VerticalLine::~VerticalLine()
+{
+}
+void VerticalLine::print()
+{
+	for (size_t i = 0; i < characters.size(); ++i)
+	{
+		cout << characters[i] << endl;
+	}
+}
---
+++ VerticalLine.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "BaseLine.h"
+
+/*
+ line that is printed verticaly, obviosly, i know
+ to print in this way endl is used
+*/
+class VerticalLine :
+	public BaseLine
+{
+public:
+	VerticalLine(int length, string _char, Position position);
+	~VerticalLine();
+
+	void print();
+};
+
